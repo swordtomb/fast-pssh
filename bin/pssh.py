f4c15ae885495f0d8aa08cbd383d0d03d90d0af7
wang <rsl4@foxmail.com>
2019-11-09 17:34:54 +0800

    add pssh simple args parse

---
+++ bin/pssh.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+
+class Foo:
+    @classmethod
+    def class_bar(cls):
+        pass
+
+    @staticmethod
+    def static_bar():
+        pass
+
+
+def do_pssh():
+    cmd = ['ssh']
+
